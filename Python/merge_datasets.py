8f361c5cf8ddf587dc3971587c4ed928ff7f875d
NoelChannayil <143276721+Noelchannayil@users.noreply.github.com>
2026-06-30 17:24:57 +0530

    Create merge_datasets.py

---
+++ Python/merge_datasets.py
@@ -0,0 +1,12 @@
+"""
+File: merge_datasets.py
+
+Project:
+E-Commerce Business Performance Analytics
+
+Description:
+This script merges multiple Olist datasets into a unified analytical dataset
+that is used for SQL analysis and Power BI dashboard development.
+
+Author: Noel Channayil
+"""
