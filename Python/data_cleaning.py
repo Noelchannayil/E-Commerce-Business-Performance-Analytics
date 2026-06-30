300bd9b0dc6de23ed678a1f8fae18bb88de4d213
NoelChannayil <143276721+Noelchannayil@users.noreply.github.com>
2026-06-30 17:23:17 +0530

    Update data_cleaning.py

--- Python/data_cleaning.py
+++ Python/data_cleaning.py
@@ -1,1 +1,13 @@
+"""
+File: data_cleaning.py
 
+Project:
+E-Commerce Business Performance Analytics
+
+Description:
+This script performs data cleaning on the raw Olist e-commerce datasets by
+handling missing values, correcting data types, removing duplicates, and
+preparing the data for further transformation and analysis.
+
+Author: Noel Channayil
+"""
