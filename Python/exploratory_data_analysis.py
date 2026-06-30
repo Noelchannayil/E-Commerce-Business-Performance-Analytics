734f3bc1cf49a7d6e4647200544a25459b9d2988
NoelChannayil <143276721+Noelchannayil@users.noreply.github.com>
2026-06-30 17:23:49 +0530

    Create exploratory_data_analysis.py

---
+++ Python/exploratory_data_analysis.py
@@ -0,0 +1,13 @@
+"""
+File: exploratory_data_analysis.py
+
+Project:
+E-Commerce Business Performance Analytics
+
+Description:
+This script performs exploratory data analysis (EDA) to understand customer
+behavior, sales performance, product trends, and data distributions before
+building the analytical model.
+
+Author: Noel Channayil
+"""
